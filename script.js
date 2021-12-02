563047eae7f33eba4008275a251d3bbed987222f
gigachad777 <aroozkhosh4@gmail.com>
2021-12-03 00:28:37 +0330

    added simple primary functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function request(targeturl){  
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", targeturl);
+    xmlHttp.send(null);
+    return xmlHttp.request_response;
+
+    /* a funtcion for sending requests to our designated url */
+}
+
+function stringcheck(input_string){
+    var regex = /^[a-z A-Z]+$/;
+    return regex.test(input_string);
+
+    /* function made for checking the only letters condition */
+}
+
+
+
+

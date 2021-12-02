2f28c83f62e109c14caa9c7ea844bc5e023afbe8
gigachad777 <aroozkhosh4@gmail.com>
2021-12-03 02:12:25 +0330

    added the submit function

--- script.js
+++ script.js
@@ -14,8 +14,24 @@ function stringcheck(input_string){
     /* function made for checking the only letters condition */
 }
 
-function recieve(){
-    var input_name = document.getElementById("name").value;
+var gender_saved = "";
+var username_saved = "";
+
+if(document.getElementById('submit').clicked == true)
+{
+    gender_status = document.querySelector('#gender-status');
+    var username = document.getElementsByName('txtJob')[0].value;
+    if (stringcheck(username) == true & gender_status == "male"){
+        gender_saved = gender_status;
+        username_saved = username;
+    }
+    if (stringcheck(username) == true & gender_status == "female"){
+        gender_saved = gender_status;
+        username_saved = username;
+    }
+    else if (stringcheck(username) == true){
+        
+    }
 }
 
 
